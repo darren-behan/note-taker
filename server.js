918eedb74c6f9b3ca0d46a730c19bee136872eba
Darren Behan <darrenbehan@hotmail.com>
2020-07-22 10:31:56 +1000

    fix to GET for index.html to ensure notes.html redners on click

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
